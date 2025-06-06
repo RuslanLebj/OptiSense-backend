36cfacfab4d7b814c6016add58f82d0397f05baa
Ruslan Lebj <dressgecko@gmail.com>
2025-06-06 19:32:18 +0500

    network and envs features

--- src/optisense/api/admin.py
+++ src/optisense/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Camera, Outlet, Record, CustomUser
+
+admin.site.register(Camera)
+admin.site.register(Outlet)
+admin.site.register(Record)
+admin.site.register(CustomUser)
